d0a54238fc7c7524b9e223999d83e7b1db9bc604
DhrushiKapadiya <dhrushikapadiya93261@gmail.com>
2025-05-02 17:15:09 +0530

    Update drapp.py

--- drapp.py
+++ drapp.py
@@ -194,7 +194,8 @@ def login():
 
 
 def register():
-    set_background("Images/backgroundlogin2.jpg")
+    set_background(Image.open("Images/backgroundlogin2.jpg"))
+
 
     st.markdown("<h1 style='text-align: center; font-size: 52px;'>📝 Register New Account</h1>", unsafe_allow_html=True)
     col1, col2, col3 = st.columns([1, 2, 1])  # Center column is wider
